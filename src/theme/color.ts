e1b38c46afe09c3e4631cd4e0a3d268d30d28b1e
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-04-04 12:13:19 -0300

    feat: add default colors

--- src/theme/color.ts
+++ src/theme/color.ts
@@ -1,3 +1,18 @@
+// Tailwind Pallette: zinc
+
 export const color = {
+  light: {
+    background: '#f4f4f5'
+  },
+  dark: {
+    background: '#18181b'
+  },
+  
+  white: '#fafafa',
+  black: '#09090b',
   primary: '#1d4ed8',
+  gray: {
+    400: '#a1a1aa',
+    600: '#71717a'
+  }
 }
