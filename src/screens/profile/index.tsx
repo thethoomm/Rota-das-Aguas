bf3f269b285dbd2ef7c7c308bcd789da3a6a56f9
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-04-04 13:23:00 -0300

    feat: create profile screen

--- src/screens/profile/index.tsx
+++ src/screens/profile/index.tsx
@@ -1,14 +1,31 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { Header, HeaderAvatar, HeaderTitle } from './_component/header';
 import { theme } from '../../theme';
+import { StatusBar } from 'expo-status-bar';
+import { profileOptions } from '../../utils/profile-options';
+import Card from './_component/card';
+import { Feather } from '@expo/vector-icons';
 
 export default function Profile() {
   return (
     <SafeAreaView style={styles.container}>
+      <StatusBar animated />
       <Header>
-        <HeaderAvatar/>
-        <HeaderTitle title='Thomas Henrique'/>
+        <HeaderAvatar />
+        <HeaderTitle title='Thomas Henrique' />
       </Header>
+
+      <ScrollView style={styles.optionList}>
+        {
+          profileOptions.map(opt => (
+            <Card key={opt.id} option={opt} />
+          ))
+        }
+        <TouchableOpacity style={styles.logoutButton} activeOpacity={0.8}>
+          <Feather name='log-out' color={theme.color.white} size={20} />
+          <Text style={styles.logoutText}>Sair da conta</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -18,6 +35,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: theme.color.white,
     paddingTop: 80,
+    paddingBottom: 20,
     paddingHorizontal: 16,
   },
+  optionList: {
+    flex: 1,
+  },
+  logoutButton: {
+    backgroundColor: theme.color.destructive,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 4,
+    paddingVertical: 12,
+    paddingHorizontal: 8,
+    borderRadius: 8,
+    marginTop: 16,
+  },
+  logoutText: {
+    fontSize: theme.fontSize.md,
+    color: theme.color.white,
+    fontWeight: '600'
+  }
 });
---
+++ src/utils/profile-options.ts
@@ -0,0 +1,16 @@
+import { OptionType } from "../screens/profile/_component/card"
+
+export const profileOptions: OptionType[] = [
+  {
+    id: 1,
+    title: 'Viagens',
+    description: 'Minhas viagens já feitas',
+    icon: 'car',
+  },
+  {
+    id: 2,
+    title: 'Dados da conta',
+    description: 'Minhas informações pessoais',
+    icon: 'sliders',
+  }
+]
