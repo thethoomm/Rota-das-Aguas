41687bd597726ccc73fd4e45eb8f57fe37db602d
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-04-04 19:55:36 -0300

    feat: add producer tab

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,13 +1,14 @@
 import { CommonTabs } from './common'
 
 import { user } from '../utils/user'
+import { ProducerTabs } from './producer'
 
 export default function TabRoutes() {
   switch (user.type) {
     case 'common':
       return <CommonTabs />
     case 'producer':
-      return 
+      return <ProducerTabs/>
     case 'admin':
       return 
     default:
