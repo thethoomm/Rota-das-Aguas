9383ce614d7e5c121e2b8e450a3ab16cca50f1cb
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-04-04 16:23:24 -0300

    style: add background color

--- src/screens/profile/_screens/account-details/index.tsx
+++ src/screens/profile/_screens/account-details/index.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { theme } from "../../../../theme";
 
 export default function AccountDetails() {
   return (
@@ -14,6 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: theme.color.white,
   }
 })
