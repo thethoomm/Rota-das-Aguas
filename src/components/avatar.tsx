dd4497618cf749779d19b377d2c652b50e38ad85
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-04-04 12:07:20 -0300

    feat: create avatar component

---
+++ src/components/avatar.tsx
@@ -0,0 +1,23 @@
+import { Image, StyleSheet, View } from 'react-native'
+
+interface AvatarProps {
+  size?: number
+}
+
+export function Avatar({ size = 50 }: AvatarProps) {
+  return (
+    <View style={styles.container}>
+      <Image 
+      style={{ width: size, height: size, borderRadius: 100 }}
+      source={{
+        uri: 'https://api.dicebear.com/8.x/thumbs/png'
+      }} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 'auto',
+  },
+})
