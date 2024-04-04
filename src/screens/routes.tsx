1a5642b657ad2a27abe0e463362b676e741aa699
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-04-04 16:25:14 -0300

    style: fix background color

--- src/screens/routes.tsx
+++ src/screens/routes.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { theme } from '../theme';
 
 export default function Routes() {
   return (
@@ -13,7 +14,7 @@ export default function Routes() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: theme.color.white,
     alignItems: 'center',
     justifyContent: 'center',
   },
