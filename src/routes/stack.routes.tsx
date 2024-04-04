e8db6583ac98c980c632742ffd6692d824a81c3f
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-04-04 16:22:22 -0300

    refactor: stack group

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -6,6 +6,8 @@ import SignUp from '../screens/auth/signup'
 import TabRoutes from './tab.routes'
 import AccountDetails from '../screens/profile/_screens/account-details'
 import Trips from '../screens/profile/_screens/trips'
+import { theme } from '../theme'
+import { Feather } from '@expo/vector-icons'
 
 export type RootStackNavigationParamsList = {
   // Root Stack
@@ -26,13 +28,30 @@ export default function StackRoutes() {
       headerShown: false
     }}>
       {/* Root Stack */}
-      <Stack.Screen name='login' component={Login}/>
-      <Stack.Screen name='signup' component={SignUp}/>
-      <Stack.Screen name='tabs' component={TabRoutes}/>
+      <Stack.Screen name='login' component={Login} />
+      <Stack.Screen name='signup' component={SignUp} />
+      <Stack.Screen name='tabs' component={TabRoutes} />
 
       {/* Profile */}
-      <Stack.Screen name='accountDetails' component={AccountDetails}/>
-      <Stack.Screen name='trips' component={Trips}/>
+      <Stack.Group screenOptions={{
+        headerShown: true,
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          fontSize: theme.fontSize.sm,
+          fontWeight: '500'
+        },
+        headerStyle: {
+          backgroundColor: theme.color.white
+        },
+        headerShadowVisible: false,
+      }}>
+        <Stack.Screen name='accountDetails' component={AccountDetails} options={{
+          title: String('Configurações').toUpperCase(),
+        }} />
+        <Stack.Screen name='trips' component={Trips} options={{
+          title: String('Viagens').toUpperCase()
+        }}/>
+      </Stack.Group>
     </Stack.Navigator>
   )
 }
