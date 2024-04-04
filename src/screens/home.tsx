fe8e51321459600a03f7452a407dabcc44c4b60c
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-04-04 09:50:49 -0300

    first commit

---
+++ src/screens/home.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: 'bold'
+  }
+});
