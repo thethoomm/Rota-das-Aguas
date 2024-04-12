1dc18ab1d2e52ce02484226ec5ef985ee8e13eef
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-04-12 13:09:48 -0300

    feat: create route list util

---
+++ src/utils/route-list.ts
@@ -0,0 +1,37 @@
+export const routeList = [
+  {
+    name: "Tour Gastronômico da Cachaça",
+    city: "Tiradentes",
+    startTime: "09:00",
+    endTime: "13:00",
+    visitDate: "2024-05-15"
+  },
+  {
+    name: "Rota dos Vinhos",
+    city: "Bento Gonçalves",
+    startTime: "10:30",
+    endTime: "16:00",
+    visitDate: "2024-06-02"
+  },
+  {
+    name: "Passeio Histórico do Centro",
+    city: "São Paulo",
+    startTime: "14:00",
+    endTime: "17:00",
+    visitDate: "2024-07-10"
+  },
+  {
+    name: "Trilha Ecológica",
+    city: "Bonito",
+    startTime: "08:30",
+    endTime: "12:00",
+    visitDate: "2024-08-20"
+  },
+  {
+    name: "City Tour Panorâmico",
+    city: "Rio de Janeiro",
+    startTime: "11:00",
+    endTime: "15:30",
+    visitDate: "2024-09-05"
+  }
+]
