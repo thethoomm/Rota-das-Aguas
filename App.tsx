09eb971dbd87bba0a8aab8c2e5d154b285ee0882
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-04-12 12:01:26 -0300

    feat: add inter font

--- App.tsx
+++ App.tsx
@@ -1,6 +1,22 @@
 import Routes from '@/routes';
+import { Inter_100Thin as InterThin, Inter_400Regular as InterRegular, Inter_500Medium as InterMedium, Inter_600SemiBold as InterSemibold, Inter_700Bold as InterBold, Inter_800ExtraBold as InterExtrabold } from '@expo-google-fonts/inter';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    InterThin,
+    InterRegular,
+    InterMedium,
+    InterSemibold,
+    InterBold,
+    InterExtrabold
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+  
+
   return (
     <Routes/>
   )
---
+++ src/theme/font.ts
@@ -0,0 +1,8 @@
+export const font = {
+  thin: 'InterThin',
+  normal: 'InterRegular',
+  medium: 'InterMedium',
+  semibold: 'InterSemibold', 
+  bold: 'InterBold',
+  extrabold: 'InterExtrabold',
+}
